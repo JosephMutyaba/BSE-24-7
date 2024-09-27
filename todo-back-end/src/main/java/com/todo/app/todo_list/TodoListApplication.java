9afbd73cedd3818ce9efbe9c9b86ce6e2b9109e3
Garton-ival <gartonival@gmail.com>
2024-09-27 10:59:33 +0300

    The work done for code checks using checkstyle and pmd

--- todo-back-end/src/main/java/com/todo/app/todo_list/TodoListApplication.java
+++ todo-back-end/src/main/java/com/todo/app/todo_list/TodoListApplication.java
@@ -3,11 +3,18 @@ package com.todo.app.todo_list;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main entry point for the Todo List Application.
+ */
 @SpringBootApplication
 public class TodoListApplication {
 
+	/**
+	 * Main method that launches the application.
+	 *
+	 * @param args command-line arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
 	}
-
 }
