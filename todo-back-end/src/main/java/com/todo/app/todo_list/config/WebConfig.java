26fda5117bbf54ee2894a5d66d5cebf222a234b6
JosephMutyaba <josephsteinson@gmail.com>
2024-10-09 18:40:52 +0300

    profiles

--- todo-back-end/src/main/java/com/todo/app/todo_list/config/WebConfig.java
+++ todo-back-end/src/main/java/com/todo/app/todo_list/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173/","http://todolist-frontend-app.s3-website-us-east-1.amazonaws.com")
+                .allowedOrigins("http://localhost:5173/","http://todolist-frontend-app.s3-website-us-east-1.amazonaws.com", "http://todolist-production-bucket.s3-website-us-east-1.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
